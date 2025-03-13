dc250d950b8772512cf95453f32bfae614b29531
chalex2k <chalex2k@gmail.com>
2025-03-13 10:34:25 +0300

    type_declaration_methods

---
+++ type_declaration_methods/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+type email string
+
+func (e email) print() {
+	fmt.Println("email:", e)
+}
+
+type abcEmail email
+
+func main() {
+	john := abcEmail("JohnDoe@abc.com")
+
+	// john.print() // john.print undefined (type abcEmail has no field or method print)
+
+	email(john).print() // email: JohnDoe@abc.com
+}
